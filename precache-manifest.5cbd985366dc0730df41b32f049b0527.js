f539c521928be065ab83e7af0b89396c3e665805
Oknanashi <oknanashi@gmail.com>
2019-03-19 16:23:33 +0300

    Updates

---
+++ precache-manifest.5cbd985366dc0730df41b32f049b0527.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b698b38c44f4d5e04742",
+    "url": "/space-todos/static/js/runtime~main.b698b38c.js"
+  },
+  {
+    "revision": "22b0c96b7af34eddcbd0",
+    "url": "/space-todos/static/js/main.22b0c96b.chunk.js"
+  },
+  {
+    "revision": "5e5f361ca24691eec464",
+    "url": "/space-todos/static/js/2.5e5f361c.chunk.js"
+  },
+  {
+    "revision": "22b0c96b7af34eddcbd0",
+    "url": "/space-todos/static/css/main.86b46689.chunk.css"
+  },
+  {
+    "revision": "d9c0a5a8bc88b52f91594381c7e06928",
+    "url": "/space-todos/index.html"
+  }
+];
